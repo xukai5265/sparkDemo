485b8628dc12d453c9f1b19e8338d4f4f51f9f57
xukai <xukai5265@126.com>
2017-03-22 23:13:06 +0800

    DirectKafka Demo

---
+++ src/main/scala/cn/xukai/spark/DirectKafkaWordCount.scala
@@ -0,0 +1,50 @@
+package cn.xukai.spark
+
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.spark.streaming.{Minutes, Seconds, StreamingContext}
+import org.apache.spark.{SparkConf}
+import org.apache.spark.streaming.kafka010._
+import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
+import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe
+
+/**
+  * Created by xukai on 17-3-22.
+  */
+object DirectKafkaWordCount {
+  def main(args: Array[String]): Unit = {
+    import org.apache.log4j.{Level,Logger}
+    Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+    Logger.getLogger("org.apache.spark.sql").setLevel(Level.WARN)
+    Logger.getLogger("org.apache.spark.streaming").setLevel(Level.WARN)
+
+    val kafkaParams = Map[String, Object](
+      "bootstrap.servers" -> "localhost:9093",
+      "key.deserializer" -> classOf[StringDeserializer],
+      "value.deserializer" -> classOf[StringDeserializer],
+      "group.id" -> "use_a_separate_group_id_for_each_stream",
+      "auto.offset.reset" -> "latest",
+      "enable.auto.commit" -> (false: java.lang.Boolean)
+    )
+
+    val sparkConf = new SparkConf().setAppName("KafkaWordCount")
+    val ssc = new StreamingContext(sparkConf, Seconds(2))
+
+
+    val topics = Array("test")
+    val stream = KafkaUtils.createDirectStream[String, String](
+      ssc,
+      PreferConsistent,
+      Subscribe[String, String](topics, kafkaParams)
+    )
+    ssc.checkpoint("hdfs://localhost:8020/tmp")
+    val messages = stream.map(record => (record.key, record.value)).map(_._2)
+    val words = messages.flatMap(_.split(" "))
+    val wordCounts = words.map(x => (x, 1L))
+      .reduceByKeyAndWindow(_ + _, _ - _, Minutes(10), Seconds(2), 2)
+
+    wordCounts.print()
+    ssc.start()
+    ssc.awaitTermination()
+
+  }
+}
